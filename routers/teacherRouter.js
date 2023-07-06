03ffdb3b462b6d48dd543bb3c63a76fd6dd49164
Subastian02@ <cobabeajaronline@gmail.com>
2023-07-06 08:33:36 +0700

    done

--- routers/teacherRouter.js
+++ routers/teacherRouter.js
@@ -5,9 +5,10 @@ const {
   getExams,
   createExam,
   exportStudentAnswers,
-  evaluateExam,
-  refreshExamToken // Add this line
-} = require('../controllers/teacherController');
+  refreshExamToken, // Add this line
+  evaluateAnswers,
+} = require("../controllers/teacherController");
+
 const {
   verifyToken,
   authorizeTeacher,
@@ -15,9 +16,32 @@ const {
 
 const router = express.Router();
 
-router.post('/exams', verifyToken, authorizeTeacher, createExam);
-router.get('/exams/:examId/export-answers/:studentId', verifyToken, authorizeTeacher, exportStudentAnswers);
-router.post('/exams/:examId/evaluate', verifyToken, authorizeTeacher, evaluateExam);
-router.post('/exams/:examId/refresh-token', verifyToken, authorizeTeacher, refreshExamToken); // Add this line
+router.get("/students", verifyToken, authorizeTeacher, getStudents);
+router.post("/students", verifyToken, authorizeTeacher, addStudents);
+
+router.post("/exams", verifyToken, authorizeTeacher, createExam);
+
+router.get("/exams", verifyToken, authorizeTeacher, getExams);
+
+router.get(
+  "/exams/:examId/export-answers/:studentId",
+  verifyToken,
+  authorizeTeacher,
+  exportStudentAnswers
+);
+
+router.post(
+  "/exams/:examId/refresh-token",
+  verifyToken,
+  authorizeTeacher,
+  refreshExamToken
+); // Add this line
+
+router.post(
+  "/exams/:examId/evaluate/:studentId",
+  verifyToken,
+  authorizeTeacher,
+  evaluateAnswers
+);
 
 module.exports = router;
